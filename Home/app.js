d53d1435a57906f6faa0972149cb9a8406aedf80
trananh-it-hust <haianhtranmanh@gmail.com>
2023-07-27 19:12:06 +0700

    27/07

--- Home/app.js
+++ Home/app.js
@@ -1,15 +1,18 @@
-const goBtn = document.querySelector("back_to_top");
-window.addEventListener('scroll', checkHeight)
-function checkHeight(){
-    if(window.scrollY > 200) {
-        goBtn.style.display = "flex"
-    }else {
-        goBtn.style.display = "none"
-    }
-}
+const canh1 = document.getElementById("canh1");
+const canh  = document.getElementById("canh");
+const item = document.getElementById("item_dau");
+canh1.addEventListener('click',()=>{
+    canh1.style.opacity = '0.5';
+    canh.style.opacity = '1';
+    canh1.style.cursor = 'default';
+    canh.style.cursor = 'pointer';
+    item.style.display = 'block';
+})
+canh.addEventListener('click',()=>{
+    canh.style.opacity = '0.5';
+    canh1.style.opacity = '1';
+    canh.style.cursor = 'default'
+    canh1.style.cursor = 'pointer'
+    item.style.display = 'none';
 
-goBtn.addEventListener('click', ()=>{
-    window.scroll({
-        top:0
-    })
 })
